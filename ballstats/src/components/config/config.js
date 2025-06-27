983a49537aa0130547fc65f4a0b8c189cd9b7d40
89329 <89329@glr.nl>
2025-06-27 08:29:59 +0200

    Create config.js

---
+++ ballstats/src/components/config/config.js
@@ -0,0 +1,13 @@
+// Actual database configuration file
+// Copy from database.js and customize with your settings
+
+const dbConfig = {
+  host: "localhost",
+  user: "basketball_user",
+  password: "your_secure_password",
+  database: "basketball_stats",
+}
+
+module.exports = dbConfig
+  
+
